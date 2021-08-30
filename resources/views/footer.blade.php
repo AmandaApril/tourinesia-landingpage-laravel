aef355b66fc02dbe4f946fc887db9fa3bbef93fa
Amanda April <amanda_april_28rpl@student.smktelkom-mlg.sch.id>
2021-08-30 09:16:10 +0700

    update-tourinesia-landingpage-laravel

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,106 @@
+<!--footer section start-->
+    <footer class="footer-section">
+        <!--footer top start-->
+        <div class="footer-top gradient-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-12">
+                        <div class="footer-nav-wrap text-white mb-0 mb-md-4 mb-lg-0">
+                            <a class="d-block" href="#">
+                                <font color="white" , size="5" , face="Times New Roman"><b>Tourinesia</b></font>
+                            </a>
+                            <p>Tourinesia adalah sebuah aplikasi mobile dalam hal virtual tour dan mendorong kemajuan UMKM lokal khususnya dalam penjualan souvenir khas daerah.</p>
+                            <ul class="list-unstyled social-list mb-0">
+                                <li class="list-inline-item"><a href="#" class="rounded"><span class="ti-facebook white-bg color-2 shadow rounded-circle"></span></a></li>
+                                <li class="list-inline-item"><a href="#" class="rounded"><span class="ti-twitter white-bg color-2 shadow rounded-circle"></span></a></li>
+                                <li class="list-inline-item"><a href="#" class="rounded"><span class="ti-linkedin white-bg color-2 shadow rounded-circle"></span></a></li>
+                                <li class="list-inline-item"><a href="#" class="rounded"><span class="ti-dribbble white-bg color-2 shadow rounded-circle"></span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-2 col-md-4 col-12">
+                        <div class="footer-nav-wrap text-white">
+                            <h5 class="text-white">Perusahaan</h5>
+                            <ul class="list-unstyled footer-nav-list mt-3">
+                                <li><a href="#" class="text-foot"><span class="ti-angle-double-right"></span> Tentang Kami</a></li>
+                                <li><a href="#" class="text-foot"><span class="ti-angle-double-right"></span> Kebijakan Privasi</a></li>
+                                <li><a href="#" class="text-foot"><span class="ti-angle-double-right"></span> Sarat & Ketentuan</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-2 col-md-4 col-12">
+                        <div class="footer-nav-wrap text-white">
+                            <h5 class="text-white">Layanan</h5>
+                            <ul class="list-unstyled footer-nav-list mt-3">
+                                <li><a href="#" class="text-foot"><span class="ti-angle-double-right"></span> Souvenir</a></li>
+                                <li><a href="#" class="text-foot"><span class="ti-angle-double-right"></span> Ticket</a></li>
+                                <li><a href="#" class="text-foot"><span class="ti-angle-double-right"></span> Virtual Tour</a></li>
+
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-5 col-md-4 col-12">
+                        <div class="footer-nav-wrap text-white">
+                            <h5 class="text-white">Kontak Kami </h5>
+                            <ul class="list-unstyled footer-nav-list mt-3">
+                                <li><a href="#" class="text-foot"><span class="ti-angle-double-right"></span> Phone: +1234567890123</a></li>
+                                <li><a href="#" class="text-foot"><span class="ti-angle-double-right"></span> Email: <a href="mailto:email@yourdomain.com" class="link-color">email@yourdomain.com</a></li>
+                                <li><a href="#" class="text-foot"><span class="ti-angle-double-right"></span> Green Living Residence Dalion 11<br>Jl. Satsui Tubun, Gadang, Kec. Sukun, Kota Malang, Jawa Timur 65149</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--footer top end-->
+
+        <!--footer copyright start-->
+        <div class="footer-bottom gray-light-bg py-3">
+            <div class="container">
+                <div class="row text-center justify-content-center">
+                    <div class="col-md-6 col-lg-5">
+                        <p class="copyright-text pb-0 mb-0">Copyrights © 2020. All rights reserved by
+                            <a href="https://themeforest.net/user/themetags" target="_blank">ThemeTags</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--footer copyright end-->
+    </footer>
+    <!--footer section end-->
+
+    <!--bottom to top button start-->
+    <button class="scroll-top scroll-to-target" data-target="html">
+    <span class="ti-angle-up"></span>
+</button>
+    <!--bottom to top button end-->
+
+
+    <!--jQuery-->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <!--Popper js-->
+    <script src="js/popper.min.js"></script>
+    <!--Bootstrap js-->
+    <script src="js/bootstrap.min.js"></script>
+    <!--Magnific popup js-->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <!--jquery easing js-->
+    <script src="js/jquery.easing.min.js"></script>
+    <!--wow js-->
+    <script src="js/wow.min.js"></script>
+    <!--owl carousel js-->
+    <script src="js/owl.carousel.min.js"></script>
+    <!--countdown js-->
+    <script src="js/jquery.countdown.min.js"></script>
+    <!--validator js-->
+    <script src="js/validator.min.js"></script>
+    <!--custom js-->
+    <script src="js/scripts.js"></script>
+</body>
+
+<!-- Mirrored from appbeats.themetags.com/index-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 12 Jul 2021 08:28:19 GMT -->
+
+</html>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+
+<!-- Mirrored from appbeats.themetags.com/index-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 12 Jul 2021 08:27:54 GMT -->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- SEO Meta description -->
+    <meta name="description" content="AppBeats app landing page template or product landing page template helps you easily create websites for your app or product,  landing page template form promotion and many more.">
+    <meta name="author" content="ThemeTags">
+    <link rel="shortcut icon" type="image/png" href="img/" />
+
+    <!-- OG Meta Tags to improve the way the post looks when you share the page on LinkedIn, Facebook, Google+ -->
+    <meta property="og:site_name" content="" />
+    <!-- website name -->
+    <meta property="og:site" content="" />
+    <!-- website link -->
+    <meta property="og:title" content="" />
+    <!-- title shown in the actual shared post -->
+    <meta property="og:description" content="" />
+    <!-- description shown in the actual shared post -->
+    <meta property="og:image" content="" />
+    <!-- image link, make sure it's jpg -->
+    <meta property="og:url" content="" />
+    <!-- where do you want your post to link to -->
+    <meta property="og:type" content="article" />
+
+    <!--title-->
+    <title>Tourinesia</title>
+
+    <!--google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700%7COpen+Sans&amp;display=swap" rel="stylesheet">
+
+    <!--Bootstrap css-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
+    <!--Magnific popup css-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/magnific-popup.css') }}">
+    <!--Themify icon css-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/themify-icons.css') }}">
+    <!--animated css-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/animate.min.css') }}">
+    <!--Owl carousel css-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/owl.theme.default.min.css') }}">
+    <!--custom css-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
+    <!--responsive css-->
+    <link rel="stylesheet" type="text/css "href="{{ asset('/css/responsive.css') }}">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.0/themify-icons.css">
+    <script src="https://kit.fontawesome.com/9329f29326.js" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+
+
+    <!--loader start-->
+    <div id="preloader">
+        <div class="loader1">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <!--loader end-->
+
+    <!--header section start-->
+    <header class="header">
+        <!--start navbar-->
+        <nav class="navbar navbar-expand-lg fixed-top bg-transparent">
+            <div class="container">
+                <a class="navbar-brand" href="index.html">
+                    <font color="white" , size="5" , face="Times New Roman"><b>Tourinesia</b></font>
+                </a>
+                <button class="navbar-toggler" tyfonte="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="ti-menu"></span>
+            </button>
+                <div class="collapse navbar-collapse h-auto" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto menu">
+                        <li class="dropdown"><a href="#"> Beranda</a></li>
+                        <li><a href="#wisata" class="page-scroll">Wisata</a></li>
+                        <li><a href="#suvenir" class="page-scroll">Suvenir</a></li>
+                        <li><a href="#contact" class="page-scroll">Kontak Kami</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!--header section end-->
